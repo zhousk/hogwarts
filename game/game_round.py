08b0d9023f36a584f8f956bec448db8aa10c1948
sky <980692186@qq.com>
2020-10-25 23:36:36 +0800

    python 实战第一次作业 refactor

--- game/game_round.py
+++ game/game_round.py
@@ -13,16 +13,19 @@ def fight(enemy_hp, enemy_power):
 
         # 判断游戏结束的条件：我方血量低于或等于0，或敌方血量低于或等于0，跳出循环
         if my_hp <= 0:
-            print(f"my remaining hp is {my_hp}")
-            print(f"enemy remaining hp is {enemy_hp}")
+            printHp(my_hp, enemy_hp)
             print("I lost")
             break
 
         elif enemy_hp <= 0:
-            print(f"my remaining hp is {my_hp}")
-            print(f"enemy remaining hp is {enemy_hp}")
+            printHp(my_hp, enemy_hp)
             print("I win")
             break
+
+# 封装一下
+def printHp(my_hp, enemy_hp):
+    print(f"my remaining hp is {my_hp}")
+    print(f"enemy remaining hp is {enemy_hp}")
 
 
 # 相当于程序入口，其他文件调用时候，name为文件的名字，本文件调用时，name为main
@@ -32,5 +35,3 @@ if __name__ == "__main__":
     enemy_power = random.randint(190, 210)
     # 调用函数
     fight(enemy_hp, enemy_power)
-
-
